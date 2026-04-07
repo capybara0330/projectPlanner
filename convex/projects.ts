34290b82e6f2703ce141ad4b01ce5c7abd0bb6d1
capybara0330 <lenashea03@gmail.com>
2026-04-06 22:08:56 -0400

    paginate the project list with usePaginatedQuery

--- convex/projects.ts
+++ convex/projects.ts
@@ -1,17 +1,21 @@
 import { query, mutation, internalMutation } from "./_generated/server";
 import { v } from "convex/values";
 import { internal } from "./_generated/api";
+import { paginationOptsValidator } from "convex/server";
 
 const CASCADE_DELETE_BATCH_SIZE = 100;
 
 //returns all projects
 export const list = query({
-  args: {},
-  handler: async (ctx) => {
+  args: {
+    paginationOpts: paginationOptsValidator,
+  },
+  handler: async (ctx, args) => {
     return await ctx.db
       .query("projects")
       .withIndex("by_deletedAt", (q) => q.eq("deletedAt", undefined))
-      .collect();
+      .order("desc")
+      .paginate(args.paginationOpts);
   },
 });
 
