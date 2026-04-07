1926b472e753baa91016c93afc8768cc5620dd72
capybara0330 <lenashea03@gmail.com>
2026-04-06 22:48:22 -0400

    updated vite.config.ts to match base path

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/project-planner/",
+  base: "/projectPlanner/",
   plugins: [tanstackRouter(), react(), tailwindcss()],
   resolve: {
     alias: {
